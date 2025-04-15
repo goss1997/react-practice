3baf0b5054b511207df7df36b8b1cb3c68b1f5ad
Goss <goss1997@naver.com>
2025-04-15 11:34:56 +0900

    게임 종료 및 승리 기능 추가, 플레이어 이름 변경 기능 구현

--- react-essentials-deep-dive/src/components/Player.jsx
+++ react-essentials-deep-dive/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
@@ -9,6 +9,9 @@ export default function Player({ initialName, symbol, isActive }) {
     // setIsEditing(!isEditing);
     // this is correct pattern
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
