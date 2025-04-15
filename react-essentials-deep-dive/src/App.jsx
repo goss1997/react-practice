396838b379a3ae73a198ccf89ce8fe56d4d1108b
Goss <goss1997@naver.com>
2025-04-15 10:32:15 +0900

    리팩토링: 게임 보드 및 플레이어 컴포넌트 추가, 게임 턴 관리 기능 구현

--- react-essentials-deep-dive/src/App.jsx
+++ react-essentials-deep-dive/src/App.jsx
@@ -1,10 +1,50 @@
+import { useState } from 'react';
 
-function App() {
-  
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
 }
 
-export default App
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players' className='highlight-player'>
+          <Player initialName='Player1' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='Player2' symbol='O' isActive={activePlayer === 'O'} />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  );
+}
+
+export default App;
