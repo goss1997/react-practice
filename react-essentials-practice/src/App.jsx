a0468b5a04d8cf31897b974f23134b2a0c4e86a7
Goss <goss1997@naver.com>
2025-04-16 14:54:35 +0900

    초기 React 애플리케이션 설정 및 기본 컴포넌트 추가

--- react-essentials-practice/src/App.jsx
+++ react-essentials-practice/src/App.jsx
@@ -1,7 +1,35 @@
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+import { useState } from 'react';
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration > 0;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((preUserInput) => {
+      return {
+        ...preUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && <p className='center'>Please enter valid values (no negative numbers)</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
